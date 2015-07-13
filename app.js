696019959a43a477836c3b437c3c0fce589c20d8
Edwin <chenrf@ucweb.com>
2015-07-14 00:02:39 +0800

    [app.js] 增加应用启动文件

---
+++ app.js
@@ -0,0 +1,105 @@
+
+var express = require("express")
+	,config = require("./config")
+	,app = express()
+	,errorHandler = require("errorhandler")
+	,log = require("./modules/log");
+
+// 日志
+var theLog = new log({
+	"name":config.name
+	,"level":config.isProd ? "warn" : "all"
+});
+var logger = theLog.getLogger("global");
+
+app.set("logger",logger);
+
+
+// 未知异常
+process.on("uncaughtException", function (err) {
+	logger.error("Uncaught exception:\n",err.stack);
+	app.listen(config.port);
+});
+
+app.disable("x-powered-by");
+
+// 请求超时控制
+app.use(function(req, res, next){
+	if(config.resTimeout){
+		res.setTimeout(config.resTimeout,function(){
+			res.send(200,{
+				"success":false
+				,"msg":"Error: Timeout,Response aborted."
+				,"result":{
+					"items":null
+				}
+			});
+			res.end();
+			req = res = next = null;
+		});
+	}
+	next();
+});
+
+var url = require("url");
+// 修复post数据
+app.use (function(req, res, next) {
+	req.rawBody = "";
+	req.setEncoding("utf-8");
+	req.on("data",function(chunk){
+		req.rawBody += chunk;
+	});
+	req.on("end",function() {
+		var tmp = url.parse("/?"+req.rawBody,true);
+		req.rawBody = tmp.query;
+		tmp = null;
+		next();
+	});
+});
+
+// 加载所有支持的类型
+var path = require("path");
+var fs = require("fs");
+var _p = path.resolve("./types");
+var files = fs.readdirSync(_p);
+var ddnsTypes = {};
+files.forEach(function(item) {
+	var tmpPath = _p + "/" + item;
+	var stats = fs.statSync(tmpPath);
+	if (!stats.isDirectory()) {
+		ddnsTypes.__defineGetter__(item.replace(".js", ""), function () {
+			return require(path.resolve(tmpPath));
+		});
+	}
+});
+
+
+app.get(/^\/$/, function(req, res){
+	res.send("hello world");
+});
+
+app.get("/ddns/:type", function(req, res){
+	console.log(req.params);
+	var handler = ddnsTypes[req.params.type];
+	var status = false;
+	if (handler) {
+		status = handler(req);
+	}
+	res.status(200).send("ddns   ",status)
+});
+
+app.get("/ddns/:type/status", function(req, res){
+	console.log(req.params);
+	res.send("ddns status");
+});
+
+app.use(errorHandler());
+
+if (require.main === module) {
+	app.listen(config.port);
+	console.log(
+		"DDNS [%s] online,listening on port %d"
+		,config.isProd ? "PRODUCTION" : "DEVELOPMENT"
+		,config.port
+	);
+}
