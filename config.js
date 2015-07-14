19634a4fc2bfd5d179e6bf918ce353bfcee12889
Edwin <chenrf@ucweb.com>
2015-07-14 11:13:42 +0800

    [config] 引入对独立配置

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 var meta = require("./package.json");
+var priveConf = require("./conf/config.json");
 
 var CONF = {
 	"name":meta.name
@@ -7,5 +8,12 @@ var CONF = {
 	,"port":40001
 	,"isProd":false
 };
+
+if (priveConf) {
+	// 合并独立配置
+	Object.keys(priveConf).forEach(function(key){
+		CONF[key] = priveConf[key];
+	});
+}
 
 module.exports = CONF;
