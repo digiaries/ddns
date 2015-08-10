0038f6b4790ca651aa4ef8a6d5c4166ab2de3475
Edwin <chenrf@ucweb.com>
2015-08-10 15:38:39 +0800

    [data] 修正数据不能正常写入的问题。增加写入时格式化数据。

--- modules/data.js
+++ modules/data.js
@@ -19,7 +19,7 @@ function get(type, key) {
 			reject();
 			return;
 		}
-		touch(file_path)
+		touch(file_path,type)
 			.then(function () {
 				fs.readFile(file_path,{"encoding":"utf8"},function (err, file) {
 					if (err || !file) {
@@ -35,6 +35,52 @@ function get(type, key) {
 }
 
 /**
+ * 写入文件
+ * @param {String}   type 数据集名称
+ * @param {String}   key  写入数据的key
+ * @param {Object}   data 写入数据
+ * @return {Object}       Promise 对象
+ */
+function set(type, key, data) {
+	return promiseFactory(function (resolve, reject) {
+		var file_path = config.data[type];
+		if (file_path) {
+			touch(file_path, type)
+				.then(function (){
+					get(type)
+						.then(function (filedata) {
+							if (key && data) {
+								if (typeof(key) === "object") {
+									filedata = data;
+								} else {
+									filedata[key] = data;
+								}
+								writeFile(file_path, filedata)
+									.then(function(){
+										resolve(filedata);
+									})
+									.catch(function(err){
+										reject(err);
+									});
+							} else {
+								resolve(filedata);
+							}
+						})
+						.catch(function(err){
+							reject(err);
+						});
+				})
+				.catch(function(err){
+					reject(err);
+				});
+		} else {
+			// 没设定的数据
+			reject();
+		}
+	});
+}
+
+/**
  * 向文件写入数据
  * @param  {String} file_path 文件地址
  * @param  {Object} filedata  数据
@@ -42,9 +88,9 @@ function get(type, key) {
  */
 function writeFile (file_path, filedata) {
 	return promiseFactory(function (resolve, reject){
-		fs.writeFile(file_path, JSON.stringify(filedata),function (err) {
+		fs.writeFile(file_path, JSON.stringify(filedata,null,4),function (err) {
 			if (err) {
-				reject();
+				reject(err);
 			} else {
 				resolve();
 			}
@@ -56,45 +102,20 @@ function writeFile (file_path, filedata) {
  * 检测文件，如有数据则写入数据
  * @param  {String} file_path 文件路径
  * @param  {String} type      数据集名称
- * @param  {String} key       数据key
- * @param  {Object} data      数据
  * @return {Object}           Promise 对象
  */
-function touch (file_path, type, key, data) {
+function touch (file_path, type) {
+	if (!file_path && type) {
+		file_path = config.data[type];
+	}
 	return promiseFactory(function (resolve, reject) {
 		fs.exists(file_path, function(exists){
 			if (exists) {
-				if (key && data) {
-					// 有数据则写入
-					get(type)
-						.then(function (filedata) {
-							filedata[key] = filedata[key] || {};
-							filedata[key] = data;
-							writeFile(file_path, filedata)
-								.then(function(){
-									resolve();
-								})
-								.catch(function(err){
-									reject(err);
-								});
-						})
-						.catch(function(err){
-							reject(err);
-						});
-				} else {
-					resolve();
-				}
+				resolve();
 			} else {
 				// 不存在
-				// 默认数据
-				var filedata = {};
-				if (key && data) {
-					// 写入数据
-					filedata[key] = data;
-				}
-
 				// 写入文件
-				writeFile(file_path, filedata)
+				writeFile(file_path, {})
 					.then(function(){
 						resolve();
 					})
@@ -106,22 +127,33 @@ function touch (file_path, type, key, data) {
 	});
 }
 
-/**
- * 写入文件
- * @param {String}   type 数据集名称
- * @param {String}   key  写入数据的key
- * @param {Object}   data 写入数据
- * @return {Object}       Promise 对象
- */
-exports.set = function (type, key, data) {
+
+exports.update = function (type, key, data) {
 	return promiseFactory(function (resolve, reject) {
 		var file_path = config.data[type];
 		if (file_path) {
-			touch(file_path, type, key, data)
-				.then(function (){
-					resolve();
+			get(type)
+				.then(function (filedata) {
+					filedata = filedata || {};
+					var updateData;
+					if (typeof(key) === "object" && !data) {
+						updateData = key;
+					} else {
+						updateData = {};
+						updateData[key] = data;
+					}
+
+					Object.keys(updateData).forEach(function(u_key){
+						filedata[u_key] = filedata[u_key] || {};
+						Object.keys(updateData[u_key]).forEach(function(data_key){
+							filedata[u_key][data_key] = updateData[u_key][data_key];
+						});
+					});
+
+					return set(type, {}, filedata);
 				})
 				.catch(function(err){
+					console.log(err);
 					reject(err);
 				});
 		} else {
@@ -129,6 +161,12 @@ exports.set = function (type, key, data) {
 			reject();
 		}
 	});
-}
+};
+
+exports.touch = function(type){
+	return touch(null,type);
+};
+
+exports.set = set;
 
 exports.get = get;
